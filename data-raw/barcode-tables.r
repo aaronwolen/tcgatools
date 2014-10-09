f496a71df0819bc0b094c9aafdfc81c712c60bd7
aaronwolen <aaron@wolen.com>
2014-10-09 17:09:21 -0400

    Tables used in barcode annotations must have a Code column

--- data-raw/barcode-tables.r
+++ data-raw/barcode-tables.r
@@ -9,4 +9,7 @@ logicals <- c("Yes" = TRUE, "No" = FALSE)
 .barcodes$dataType$Available     <- .barcodes[.barcodes$dataType$Available]
 .barcodes$platformCode$Available <- .barcodes[.barcodes$platformCode$Available]
 
+# Tables used in barcode annotations must have a Code column
+names(.barcodes$tissueSourceSite)[1] <- "Code"
+
 save(.barcodes, file = "R/sysdata.rda")
